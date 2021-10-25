b65f57661f7174db2a05ca7616d31932d56f9db0
LindZCoding <Lindzgaming@gmail.com>
2021-10-25 12:49:12 -0700

    meteors moving on their own down the y axis

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const game = document.getElementById("canvas");
 //movement tracker
 const moveDisplay = document.getElementById("movement")
 
+//canvas sizing
 canvas.width = 400;
 canvas.height = 600;
 
@@ -15,107 +16,115 @@ game.setAttribute("height", getComputedStyle(game)["height"])
 //we do this with the built in canvas method, getContext
 const ctx = game.getContext("2d")
 
-function Crawler(x, y, color, width, height) {
+function Meteor(x, y, color, width, height) {
     this.x = x
     this.y = y
     this.color = color
     this.width = width
     this.height = height
     this.alive = true
-    //then we declare the same type of render method
     this.render = function () {
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
-}      
+}    
 
-let player = new Crawler(380, 540, "#6FC9E7", 60, 60)
-let ogre = new Crawler(200, 50, "lightgreen", 32, 48)
+function Player(x, y, color, width, height) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.width = width
+    this.height = height
+    this.alive = true
+    this.render = function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}  
+
+let player = new Player(370, 540, "#6FC9E7", 60, 60)
+let meteorOne = new Meteor(0, -60, "pink", 60, 60)
+let meteorTwo = new Meteor(185, -60, "pink", 60, 60)
+let meteorThree = new Meteor(370, -60, "pink", 60, 60)
+let meteorFour = new Meteor(555, -60, "pink", 60, 60)
+let meteorFive = new Meteor(740, -60, "pink", 60, 60)
 console.log("this is the player", player)
-console.log("this is  the ogre", ogre)
+console.log("this is  the meteor", meteorOne)
 
-//HERE WE ARE GOING to set up our movement handler
-//the movement handler will be an event listener
-//we will use the WASD keys to move the player around the canvas
-// we are going to use switch case here but you can also use if else
-//first way we are handling input is going to be with keycodes
-// const movementHandler = (e) => {
-//     switch(e.keyCode) {
-//         //first case 87 = w key
-//         //this will move our player up
-//         case (87): 
-//             player.y -= 10
-//             break
-//         //keycode 65 = A key to the left
-//         case (65):
-//             player.x -= 10
-//             break
-//         //keycode 83 = S key moves player down
-//         case (83):
-//             player.y += 10
-//             break
-//         //keycode 68 = D key moves player right
-//         case (68):
-//             player.x += 10
-//             break
-//     }
-// }
+//array of meteors
+let meteors = [meteorOne, meteorTwo, meteorThree, meteorFour, meteorFive]
 
-//the second way we can handle key events is with e.key
 //if we want to restrict our player from leaving the canvas, we can include that in our movehandler
 
 let movementHandler = (e) => {
     switch(e.key.toLowerCase()) {
-        // case "w":
-        //     //move up
-        //     player.y -= 10
-        //     if (player.y <= 0) {
-        //         player.y = 0
-        //     }
-        //     break
         case "a":
             //move left
-            player.x -= 95
+            player.x -= 185
             if (player.x <= 0) {
                 player.x = 0
             }
             break
-        // case "s":
-        //     //move down
-        //     player.y += 10
-        //     if (player.y + player.height >= game.height) {
-        //         player.y = game.height - player.height
-        //     }
-        //     break
         case "d":
             //move right
-            player.x += 95
+            player.x += 185
             if (player.x + player.width >= game.width) {
                 player.x = game.width - player.width
             }
             break
         
     }
+
+
 }
 
+let meteorMovement = (e) => {
+    meteorOne.y += 1;
+    meteorTwo.y += 1;
+    meteorThree.y += 1;
+    meteorFour.y += 1;
+    meteorFive.y += 1;
+}
+
+let duplicate = () => {
+    if (meteorOne.y >= 600) {
+        return meteorOne.y = -60;
+    }
+    if (meteorTwo.y >= 600) {
+        return meteorTwo.y = -60;
+    }
+    if (meteorThree.y >= 600) {
+        return meteorThree.y = -60;
+    }
+    if (meteorFour.y >= 600) {
+        return meteorFour.y = -60;
+    }
+    if (meteorFive.y >= 600) {
+        return meteorFive.y = -60;
+    }
+}
+
+
 // make collision detection
-//writing some logic that determines if any part of our player square touches any part of our ogre
+//writing some logic that determines if any part of our player square touches any part of our meteor
 const detectHit = () => {
-    //if the player's x + width or y + height hits the ogre's x+width or y+height, kill shrek
+    //if the player's x + width or y + height hits the meteors x+width or y+height, kill player
+     for (i=0; i < meteors.length; i++)
     if (
-        player.x < ogre.x + ogre.width && 
-        player.x + player.width > ogre.x &&
-        player.y < ogre.y + ogre.height &&
-        player.y + player.height > ogre.y
+        player.x < meteors[i].x + meteors[i].width && 
+        player.x + player.width > meteors[i].x &&
+        player.y < meteors[i].y + meteors[i].height &&
+        player.y + player.height > meteors[i].y
     ) {
-        //kill shrek
-        ogre.alive = false
+        //kill meteor
+        player.alive = false
         //end the game
-        document.querySelector("#btm-right > h2").innerText = "You win!"
+        document.querySelector("#btm-right > h2").innerText = "You lose!"
         //not quite where we want to stop the gameLoop
         // stopGameLoop()
     }
 }
+
 // we are going to set up our game loop to be used in our timing function
 //set up gameLoop func, declaring what happens when our game is running
 const gameLoop = () => {
@@ -123,15 +132,32 @@ const gameLoop = () => {
         ctx.clearRect(0, 0, game.width, game.height)
         //display relevant game state(player movement) in our movement display
         moveDisplay.innerText = `X: ${player.x}\nY: ${player.y}`
-        //check if the ogre is alive, if so render the ogre
-        if (ogre.alive) {
-            ogre.render()
+        //check if the meteor is alive, if so render the meteor
+        if (player.alive) {
+            meteorOne.render()
+            meteorTwo.render()
+            meteorThree.render()
+            meteorFour.render()
+            meteorFive.render()
             //add in our detection to see if the hit has been made 
             detectHit()
         } 
+
+        duplicate();
+
         //render our player
-        player.render()
+        if (player.alive) {
+        player.render();
+        }
+
+        //down movement function on the y axis of the meteors
+        meteorMovement();
+
+        setInterval(meteorMovement, 200);
+
 }
+
+// duplicate();
 //we also need to declare a func that will stop our animation loop
 let stopGameLoop = () => {clearInterval(gameInterval)}
 
